99ed5a77260c010f73a6a20bddf98c12b7d3470c
Aditya Tukaram Shelar <152534139+Aditya285-S@users.noreply.github.com>
2024-12-22 22:12:19 +0530

    Update urls.py

--- marvelverse/urls.py
+++ marvelverse/urls.py
@@ -25,4 +25,6 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    # urlpatterns += + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
